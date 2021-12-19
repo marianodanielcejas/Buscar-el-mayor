0167cf23f6aa1cd6a72769d5deca7e3c9d8777f4
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2021-12-19 20:47:55 -0300

    Agregar archivos de proyecto.

---
+++ Buscar el mayor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscar_el_mayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            int TomaNumero = 0;
+            int NumeroMaximo = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write("Ingrese un Número por favor: ");
+                Num = int.Parse(Console.ReadLine());
+                if(TomaNumero == 0)
+                {
+                    TomaNumero = 1;
+                    NumeroMaximo = Num;
+                }
+                else
+                {
+                    if(Num >= NumeroMaximo)
+                    {
+                        NumeroMaximo = Num;
+                    }
+                }
+            }
+            Console.WriteLine("El Número maximo ingresado es: {0} ", NumeroMaximo);
+            Console.Read();
+        }
+    }
+}
